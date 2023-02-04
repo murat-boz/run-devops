4bc13101b482a85c437fcfe42be12f79bd96ff17
murat-boz <muratboz@hotmail.com>
2023-02-04 23:54:46 +0300

    Shopping.API development is finished.

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Models;
+using System.Collections.Generic;
+using System;
+using Shopping.API.Data;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductContext.Products;
+        }
+    }
+}
